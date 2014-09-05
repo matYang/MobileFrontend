c4e1d7bca2ee450bae65b5f68a343a96e48c2028
jzlingmo <120237546@qq.com>
2014-09-05 16:27:41 +0800

    school api

--- app/services/restAPI.js
+++ app/services/restAPI.js
@@ -64,17 +64,63 @@ appServices.factory('restAPI',
             'makeResourceUrl': makeResourceUrl//资源的地址
         };
     }]
-)
-    .factory('getSchool', ['$resource', 'app',
-        function ($resource, app) {
-
+)//带有缓存服务的promise服务 仅限GET请求
+    .factory('promiseGet', ['$q',
+        function ($q) {
+            return function (param, restAPI, cacheId, cache) {
+                var result, defer = $q.defer();
+                //如果指定了cacheId和cache则会优先获取缓存中值
+                result = cacheId && cache && cache.get(cacheId);
+                if (result) {
+                    defer.resolve(result);
+                } else {
+                    restAPI.get(param, function (data) {
+                        if (cacheId && cache) {
+                            cache.put(cacheId, data);
+                        }
+                        defer.resolve(data);
+                    }, function (data) {
+                        defer.reject(data.error);
+                    });
+                }
+                return defer.promise;
+            };
         }
-    ]).factory('getCategory', ['$resource', 'app',
-        function ($resource, app) {
-
+    ])
+    //缓存服务
+    .factory('cache', ['$cacheFactory',
+        function ($cacheFactory) {
+            return {
+                //课程分类目录的缓存 容量大小为200条记录
+                category: $cacheFactory('category', {
+                    capacity: 200
+                }),
+                location: $cacheFactory('location', {
+                    capacity: 200
+                })
+            };
         }
-    ]).factory('getSchool', ['$resource', 'app',
-        function ($resource, app) {
-
+    ])
+    //返回课程目录数据 使用promiseGet对数据使用内存缓存
+    .factory('getCategory', ['restAPI', 'cache', 'promiseGet',
+        function (restAPI, cache, promiseGet) {
+            return function () {
+                return promiseGet({}, restAPI.category, 'category', cache.category);
+            };
+        }
+    ])
+    .factory('getLocation', ['restAPI', 'cache', 'promiseGet',
+        function (restAPI, cache, promiseGet) {
+            return function () {
+                return promiseGet({}, restAPI.location, 'location', cache.location);
+            };
+        }
+    ])
+    //仅查询enabled=1的school
+    .factory('getSchool', ['restAPI', 'cache', 'promiseGet',
+        function (restAPI, cache, promiseGet) {
+            return function (id) {
+                return promiseGet({enabled:1}, restAPI.school, null, null);
+            };
         }
     ]);
