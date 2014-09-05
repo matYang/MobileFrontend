88fd9313d7f93aece7a0137074426dc80177850f
jzlingmo <120237546@qq.com>
2014-09-06 00:00:58 +0800

    a full resolution of send sms

--- app/controllers/registerCtrl.js
+++ app/controllers/registerCtrl.js
@@ -2,15 +2,36 @@
  * Created by jz on 2014/9/3.
  */
 app.controller('registerCtrl', ['$scope', 'app', 'Auth', function ($scope, app, Auth) {
+    //发送短信按钮的状态
+    //todo 发送验证码
+    //发送状态
+    //按钮状态status
+    //#0 点击发送 >>可点击
+    //#1 发送中倒计时 重新发送 count >>不可点击
+    //#2 倒计时结束 重新发送 >>可点击
+    //#3 发送失败 重新发送 >>可点击
 
-
+    // 正在发送 发送成功(倒计时)  发送失败
+    // 重新发送
+    $scope.status = 0;
     $scope.sendSms = function (e) {
-        alert('send sms')
+        //仅1状态下不可点击
+        if($scope.status == 1)return;
+        console.log('send sms');
+        //success
+        $scope.status = 1;
+        $scope.count = 5;
+        var countdownPromise = app.$interval(function(){
+            console.log($scope.count -- );
+            if($scope.count <= 0 ){
+                app.$interval.cancel(countdownPromise);
+                $scope.status = 2;
+                console.log('can retry')
+            }
+        },1000);
+        //error
+//        $scope.status = 3;
 
-        //todo 发送验证码
-        //发送状态
-        // 正在发送 发送成功(倒计时)  发送失败
-        // 重新发送
 
         //    $info.html("验证码已经发送至您的手机");
 //    var count_down = function (k) {
