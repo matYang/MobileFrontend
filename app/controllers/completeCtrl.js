c64732a784b1014732f46e1e08dc4dada230119f
jzlingmo <120237546@qq.com>
2014-09-05 17:46:21 +0800

    can choose school

--- app/controllers/completeCtrl.js
+++ app/controllers/completeCtrl.js
@@ -10,16 +10,25 @@ app.controller('completeCtrl', ['$scope', 'app', function ($scope, app) {
 
     //提交完善的信息 学校信息
     $scope.submitComplete = function (schoolId) {
+        if (!schoolId) {
+            //todo 提醒用户未选择学校
+            alert('未选择学校');
+            return
+        }
         var id = app.$rootScope.global.user && app.$rootScope.global.user.id;
         if (!id) {
             //todo 提醒用户未进行登录 点击确定后进入登录页面
+            alert('尚未登录');
+//            return
         }
-        if (!schoolId) {
-            //todo 提醒用户未选择学校
-        }
+
         console.log('completing');
         restAPI.update({ID: 'info'}, {id: id, schooleId: schoolId}, function () {
-
+            alert('success');
+            app.$state.go('courseList');
+        },function(){
+            //todo error message
+            alert('error')
         })
     }
 }]);
