c06702dfdaa7bd876537e6fb2f19bdf55defc475
Jet <120237546@qq.com>
2014-09-17 23:22:15 +0800

    response error message

--- app/controllers/applyCtrl.js
+++ app/controllers/applyCtrl.js
@@ -3,9 +3,7 @@ appControllers.controller('applyCtrl', ['$scope', '$ionicModal', 'app',
         /**
          * 助学贷款和人工选课的共同controller
          */
-
-        //保存选择课程三级类别的对obj
-        $scope.chosen = {};
+        $scope.chosen = {};//保存选择课程三级类别的obj（obj.name obj.id）
         $scope.apply = {
             phone: app.$rootScope.global.user && app.$rootScope.global.user.phone,
             userName: app.$rootScope.global.user && app.$rootScope.global.user.name
@@ -31,20 +29,31 @@ appControllers.controller('applyCtrl', ['$scope', '$ionicModal', 'app',
         $scope.submitApply = function () {
             var apply = angular.copy($scope.apply);
             apply.categoryId = $scope.chosen.cat && $scope.chosen.cat.id;
-            //todo 验证用户输入
-            if (apply.categoryId === undefined) {
+            //验证用户输入
+            if (!apply.categoryId) {
                 app.alert('请选择您的课程意向');
                 return;
             }
+            if (!apply.phone) {
+                app.alert('请输入联系电话');
+                return;
+            }
+            if (apply.phone.length != 11 || isNaN(parseInt(apply.phone, 10))) {
+                alert('手机号格式不正确');
+                return
+            }
+            if (!apply.userName) {
+                app.alert('请输入您的姓名');
+                return;
+            }
+            //如果处于助学贷款页面 添加备注
             if (app.$state.is('applyLoan')) {
-                //如果处于助学贷款页面 添加备注
                 apply.remark = (apply.remark || '') + '[申请助学贷款]';
             }
             app.restAPI.Apply.save(apply, function () {
-                //todo success
                 app.$state.go('applySuccess');
-            }, function () {
-                //todo error
+            }, function (response) {
+                app.alert(response.data.message || '申请失败，请稍后再试');
             })
         }
     }
